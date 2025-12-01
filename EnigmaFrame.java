56217a79095d3cf68a02381d77d885074b2ba560
Carol Lu <Carol10051113@gmail.com>
2025-12-01 05:46:38 -0500

    basic

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -1,3 +1,19 @@
-public class EnigmaFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class EnigmaFrame extends JFrame {
+
+    private JComboBox<Integer> innerBox;
+    private JComboBox<Integer> middleBox;
+    private JComboBox<Integer> outerBox;
+    private JTextField startField;
+    private JTextArea inputArea;
+    private JTextArea outputArea;
+    private JButton encryptButton;
+    private JButton decryptButton;
+
+    
 
 }
